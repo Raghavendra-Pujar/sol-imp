7ba61d5a3116d91aaeeec0829505d8213525981c
Raghavendra-Pujar <34229618+Raghavendra-Pujar@users.noreply.github.com>
2020-01-01 16:10:48 +0530

    Update get-booking.service.ts

--- get-booking/get-booking.service.ts
+++ get-booking/get-booking.service.ts
@@ -1,1 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { BuffetBooking } from '../buffet-booking/buffet-booking';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GetBookingService {
+
+  constructor(private http: HttpClient) { }
+
+  getBooking(id): Observable<BuffetBooking>{
+    return this.http.get<BuffetBooking>("http://localhost:3000/fetchBooking/"+id);
+  }
+}
 
